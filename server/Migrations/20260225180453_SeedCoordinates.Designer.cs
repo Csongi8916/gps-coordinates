15b32b84e18385ada812012633c51772786ef2d5
Komár Csongor <csongi8916@gmail.com>
2026-02-25 19:19:02 +0100

    Seed base coordinates

--- server/Migrations/20260225180453_SeedCoordinates.Designer.cs
+++ server/Migrations/20260225180453_SeedCoordinates.Designer.cs
@@ -2,6 +2,7 @@
 using GpsCoordinatesApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GpsCoordinatesApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225180453_SeedCoordinates")]
+    partial class SeedCoordinates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/Migrations/20260225180453_SeedCoordinates.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GpsCoordinatesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCoordinates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coordinates",
+                columns: new[] { "Id", "Description", "Latitude", "Longitude", "Name", "OrderIndex" },
+                values: new object[,]
+                {
+                    { 1, null, 47.497900000000001, 19.040199999999999, "Start Budapest", 1 },
+                    { 2, null, 47.4985, 19.042000000000002, "Point 2", 2 },
+                    { 3, null, 47.5, 19.045000000000002, "Point 3", 3 },
+                    { 4, null, 47.500999999999998, 19.047999999999998, "Point 4", 4 },
+                    { 5, null, 47.502000000000002, 19.050000000000001, "Point 5", 5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coordinates",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coordinates",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Coordinates",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Coordinates",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Coordinates",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
