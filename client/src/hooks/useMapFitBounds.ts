d6becc388c4a0b69255982a82bcb5888151715cd
Komár Csongor <csongi8916@gmail.com>
2026-02-27 11:55:36 +0100

    Feat004: Fix typo

--- client/src/hooks/useMapFitBounds.ts
+++ client/src/hooks/useMapFitBounds.ts
@@ -5,18 +5,18 @@ import { LatLngBounds, type LatLngTuple } from "leaflet"
 export const useMapFitBounds = (positions: LatLngTuple[]) => {
   const map = useMap()
 
-const hasFitRef = useRef(false)
+  const hasFitRef = useRef(false)
 
-useEffect(() => {
-  if (!positions.length) return
-  if (hasFitRef.current) return
+  useEffect(() => {
+    if (!positions.length) return
+    if (hasFitRef.current) return
 
-  const bounds = new LatLngBounds(positions)
+    const bounds = new LatLngBounds(positions)
 
-  map.fitBounds(bounds, {
-    padding: [150, 150]
-  })
+    map.fitBounds(bounds, {
+      padding: [150, 150]
+    })
 
-  hasFitRef.current = true
-}, [positions, map])
+    hasFitRef.current = true
+  }, [positions, map])
 }
